be1223f5965a476c7d813b55770db33fee1bd44d
DHANUKA <danuka1999lakshan@gmail.com>
2021-09-04 15:02:53 +0530

    variable initializing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    static int x = 10;
+    int y = 10;
+    x=x+1;
+    y=y+1;
+    printf("%d \n",x);
+    printf("%d",y);
     return 0;
 }
