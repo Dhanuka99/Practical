21a57df15e604be4d75ad122bc5e4ae364d83d10
DHANUKA <danuka1999lakshan@gmail.com>
2021-09-07 13:10:10 +0530

    call by value

---
+++ C/Test1.cpp
@@ -0,0 +1,25 @@
+//
+// Created by DHANUKA on 9/7/2021.
+//
+
+#include <stdio.h>
+
+int addNumber(int,int);
+int main(){
+    int a=10,b=20;
+    printf("%d %d\n",a, b);
+    int sum = addNumber(a,b);
+    printf("%d \n",sum);
+    printf("%d %d\n",a, b);
+    return 0;
+}
+int addNumber(int x,int y){
+    x=1000;
+    y=2000;
+    return x+y;
+}
+
+/*10 20
+3000
+10 20*/
+
